0a3499a7500e82f416fd49c51cdd7d6b58ff8167
Peter Erwin <erwin@sigmaxi.net>
2017-05-12 22:08:52 +0200

    Added simulated-survey code

---
+++ make_simulated_surveys.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+# Script for generating simulated surveys for measuring bar fractions as a 
+# function of stellar mass or gas mass ratio. Outputs are stored in the
+# directory specified by baseDirSurv.
+
+from __future__ import print_function
+import simulate_surveys
+
+# output directory for simulation results
+baseDirSurv = "./data/"
+
+
+colHeaders_logmstar = "# logMstar	medFbar		sigma_low	sigma_high\n"
+colHeaders_logfgas = "# log_fgas	medFbar		sigma_low	sigma_high\n"
+
+
+# random-number-generator seed used for simulations in paper; change for
+# different pseudorandom number sequences (set = None to base seed on
+# current time)
+randomSeed = 100
+
+
+
+# * Simulations measuring f_bar as a function of stellar mass for GZ2-style surveys
+# GZoo -- M_star, D < 30 Mpc:
+n = 10000
+nFWHM = 2.0
+zrange_mstar = [0.01, 0.05]
+
+mstar_bincenters, fmed, flow, fhigh = simulate_surveys.GenerateAndObserveNTimes(200, simulate_surveys.dset_d30_sp, 
+zrange_mstar, n, nFWHM*1.4, 9.0, 11.25, 0.25, useObservedSizes=False, randomSeed=randomSeed)
+sigma_minus_2fwhm = fmed - flow
+sigma_plus_2fwhm = fhigh - fmed
+ff = baseDirSurv + "sim_logMstar_d30_sp_2xfwhm_SDSS_200_dp-sizes.txt"
+outf = open(ff, 'w')
+outf.write(colHeaders_logmstar)
+for i in range(len(fmed)):
+	outf.write("%.2f   %.4f   %.4f   %.4f\n" % (mstar_bincenters[i], fmed[i], sigma_minus_2fwhm[i], sigma_plus_2fwhm[i]))
+outf.close()
+print("Output saved to %s." % ff)
+
+
+# * Simulations measuring f_bar as a function of gas mass ratio for GZ2-style surveys
+# GZoo -- f_gas, D < 30 Mpc:
+n = 2000
+zrange_fgas = [0.01,0.05]
+
+fgas_bincenters, fmed, flow, fhigh = simulate_surveys.GenerateAndObserveNTimes(200, simulate_surveys.dset_d30m95_sp, 
+zrange_fgas, n, nFWHM*1.4, -2,1,0.5, value='fgas', useObservedSizes=False, useHILimit=False, randomSeed=randomSeed, debug=True)
+sigma_minus_2fwhm = fmed - flow
+sigma_plus_2fwhm = fhigh - fmed
+ff = baseDirSurv + "sim_logfgas_d30_2xfwhm_SDSS_200_dp-sizes.txt"
+outf = open(ff, 'w')
+outf.write(colHeaders_logfgas)
+for i in range(len(fmed)):
+	outf.write("%.2f   %.4f   %.4f   %.4f\n" % (fgas_bincenters[i], fmed[i], sigma_minus_2fwhm[i], sigma_plus_2fwhm[i]))
+outf.close()
+print("Output saved to %s." % ff)
+
+# Same, but now including simple H I detection threshold
+fgas_bincenters, fmed, flow, fhigh = simulate_surveys.GenerateAndObserveNTimes(200, simulate_surveys.dset_d30m95_sp, 
+zrange_fgas, n, nFWHM*1.4, -2,1,0.5, value='fgas', useObservedSizes=False, useHILimit=True, randomSeed=randomSeed, debug=True)
+sigma_minus_2fwhm = fmed - flow
+sigma_plus_2fwhm = fhigh - fmed
+ff = baseDirSurv + "sim_logfgas_d30m95_sp_2xfwhm_SDSS_200_HI-limited.txt"
+outf = open(ff, 'w')
+outf.write(colHeaders_logfgas)
+for i in range(len(fmed)):
+	outf.write("%.2f   %.4f   %.4f   %.4f\n" % (fgas_bincenters[i], fmed[i], sigma_minus_2fwhm[i], sigma_plus_2fwhm[i]))
+outf.close()
+print("Output saved to %s." % ff)
+
+
+
+# * Simulation for f_bar as a function of stellar mass in an 
+# HST-style survey, assuming all galaxies at z = 0.75
+zrange_075 = [0.75,0.75]
+mstar_bincenters, fmed, flow, fhigh = simulate_surveys.GenerateAndObserveNTimes(200, simulate_surveys.dset_d30_sp, 
+zrange_075, n, nFWHM*0.1, 9.0, 11.5, 0.25, useObservedSizes=False)
+sigma_minus_2fwhm = fmed - flow
+sigma_plus_2fwhm = fhigh - fmed
+ff = baseDirSurv + "sim_logMstar_d30_sp_2xfwhm_HST_z0.75_200_dp-sizes.txt"
+outf = open(ff, 'w')
+outf.write(colHeaders_logmstar)
+for i in range(len(fmed)):
+	outf.write("%.2f   %.4f   %.4f   %.4f\n" % (mstar_bincenters[i], fmed[i], sigma_minus_2fwhm[i], sigma_plus_2fwhm[i]))
+outf.close()
+print("Output saved to %s." % ff)
+
+# Same, but now assuming all bars have half their z=0 size
+mstar_bincenters, fmed, flow, fhigh = simulate_surveys.GenerateAndObserveNTimes(200, simulate_surveys.dset_d30_sp, 
+zrange_075, n, nFWHM*0.1, 9.0, 11.5, 0.25, useObservedSizes=False, scaleSizes=0.5)
+sigma_minus_2fwhm = fmed - flow
+sigma_plus_2fwhm = fhigh - fmed
+ff = baseDirSurv + "sim_logMstar_d30_sp_2xfwhm_HST_z0.75_200_dp-sizes_scale0.5.txt"
+outf = open(ff, 'w')
+outf.write(colHeaders_logmstar)
+for i in range(len(fmed)):
+	outf.write("%.2f   %.4f   %.4f   %.4f\n" % (mstar_bincenters[i], fmed[i], sigma_minus_2fwhm[i], sigma_plus_2fwhm[i]))
+outf.close()
+print("Output saved to %s." % ff)
+
--- s4gutils.py
+++ s4gutils.py
@@ -1,5 +1,7 @@
 # Miscellaneous code for analysis of S4G bar fractions
 
+import copy
+import math
 import random
 import numpy as np
 
@@ -56,5 +58,67 @@ def ConfidenceInterval( vect ):
 	vect_sorted = copy.copy(vect)
 	vect_sorted.sort()
 	return (vect_sorted[lower_ind], vect_sorted[upper_ind])
+
+
+def Binomial( n, n_tot, nsigma=1.0, conf_level=None, method="wilson" ):
+	"""Computes fraction (aka frequency or rate) of occurances p = (n/n_tot).
+	Also computes the lower and upper confidence limits using either the 
+	Wilson (1927) or Agresti & Coull (1998) method (method="wilson" or method="agresti");
+	default is to use Wilson method.
+	Default is to calculate 68.26895% confidence limits (i.e., 1-sigma in the
+	Gaussian approximation).
 	
+	Returns tuple of (p, sigma_minus, sigma_plus).
+	"""
+	
+	# the following works only for scalar inputs
+# 	if (n_tot == 0):
+# 		return (0.0, 0.0, 0.0)
+	
+	p = (1.0 * n) / n_tot
+	q = 1.0 - p
+	
+	if (conf_level is not None):
+		print("Alternate values of nsigma or conf_limit not yet supported!")
+		alpha = 1.0 - conf_level
+		# R code would be the following:
+		#z_alpha = qnorm(1.0 - alpha/2.0)
+		return None
+	else:
+		z_alpha = nsigma   # e.g., z_alpha = nsigma = 1.0 for 68.26895% conf. limits
+	
+	if (method == "wald"):
+		# Wald (aka asymptotic) method -- don't use except for testing purposes!
+		sigma_minus = sigma_plus = z_alpha * np.sqrt(p*q/n_tot)
+	else:
+		z_alpha2 = z_alpha**2
+		n_tot_mod = n_tot + z_alpha2
+		p_mod = (n + 0.5*z_alpha2) / n_tot_mod
+		if (method == "wilson"):
+			# Wilson (1927) method
+			sigma_mod = np.sqrt(z_alpha2 * n_tot * (p*q + z_alpha2/(4.0*n_tot))) / n_tot_mod
+		elif (method == "agresti"):
+			# Agresti=Coull method
+			sigma_mod = np.sqrt(z_alpha2 * p_mod * (1.0 - p_mod) / n_tot_mod)
+		else:
+			print("ERROR: method \"%s\" not implemented in Binomial!" % method)
+			return None
+		p_upper = p_mod + sigma_mod
+		p_lower = p_mod - sigma_mod
+		sigma_minus = p - p_lower
+		sigma_plus = p_upper - p
+	
+	return (p, sigma_minus, sigma_plus)
+
+
+def ConfidenceInterval( vect ):
+	
+	nVals = len(vect)
+	lower_ind = int(round(ONESIGMA_LOWER*nVals)) - 1
+	upper_ind = int(round(ONESIGMA_UPPER*nVals))
+	vect_sorted = copy.copy(vect)
+	vect_sorted.sort()
+	return (vect_sorted[lower_ind], vect_sorted[upper_ind])
+	
+
 
