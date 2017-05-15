6e432f3795333f3bf82fdd79911a44bc4e0e6bfe
Peter Erwin <erwin@sigmaxi.net>
2017-05-16 00:43:19 +0200

    Added PlotFrequency to plotutils.py; added Figure A1 to s4gbars_main.ipynb; minor update to README

--- plotutils.py
+++ plotutils.py
@@ -95,3 +95,62 @@ def PlotFrequencyWithWeights( values, weights, i_1, i_non1, start, stop, step, o
 					capthick=1.2, capsize=5, **kwargs)
 
 	return (midvals, frac_1, delta_low, delta_high)
+
+
+def PlotFrequency( values, i_1, i_non1, start, stop, step, offset=0, noErase=False, 
+					axisObj=None, debug=False, **kwargs ):
+					
+	"""
+	Plots frequencies with binomial 68% confidence intervals (as error bars) in
+	bins of values, where i_1 are indices defining detections and i_non1 define
+	non-detections.
+
+	values = vector of values (e.g., M_star)
+	i1 = list of indices into values for objects in parent sample 
+		and in group 1 (e.g., barred galaxies)
+	i_non1 = list of indices into values for objects in parent sample 
+		but *not* in group 1 (e.g., unbarred galaxies)
+	start, stop, step = start, stop, and spacing for bins
+	
+	offset = optional additive offset to x [values] positions of plotted points
+	
+	axisObj = optional matplotlib Axes object where plot will be placed
+	
+	**kwargs = extra keyword=value pairs passed to plt.errorbar
+
+	Example:
+	PlotFrequency(logMstar, ii_boxy, ii_nonboxy, 9.0, 11.6, 0.2)
+	"""
+	binranges = np.arange(start, stop, step)
+	n1,bins = np.histogram(np.array(values)[i_1], bins=binranges)
+	n2,bins = np.histogram(np.array(values)[i_non1], bins=binranges)
+	nBins = len(n1)
+
+	ff = []
+	ff_low = []
+	ff_high = []
+	for i in range(nBins):
+		if n1[i] + n2[i] == 0:
+			f = f_low = f_high = np.nan
+		else:
+			f,f_low,f_high = s4gutils.Binomial(n1[i], n1[i] + n2[i])
+		ff.append(f)
+		ff_low.append(f_low)
+		ff_high.append(f_high)
+	x = np.array([0.5*(bins[i] + bins[i + 1]) for i in range(len(bins) - 1)])
+	if debug:
+		print(bins)
+		print("n1 (i_1) = ", n1)
+		print("n2 (i_non1) = ", n2)
+		print(ff)
+	
+	if noErase is False:
+		plt.clf()
+	if axisObj is None:
+		plt.errorbar(x + offset, ff, [ff_low,ff_high], None, elinewidth=1.2, 
+					capthick=1.2, capsize=5, **kwargs)
+	else:
+		axisObj.errorbar(x + offset, ff, [ff_low,ff_high], None, elinewidth=1.2, 
+					capthick=1.2, capsize=5, **kwargs)
+
+
